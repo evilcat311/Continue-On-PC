26990c3690a55f4601dafd886a808c2a8c26d9e8
connor <evilcat311@gmail.com>
2019-10-24 01:11:26 -0400

    Skell out imports and initializing variables needed later

---
+++ src/Receive_Mail.java
@@ -0,0 +1,16 @@
+import java.util.Properties;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+import javax.mail.Store;
+
+public class Receive_Mail {
+    public static void main(String[] args){
+        String Host_Email = "TestMail.gmail.com";
+        String mailStrProt = "pop3";
+        String Username = "evilcat311@gmail.com";
+
+    }
+}
